7cfbbceaaf0add87137698ab6c81ebce874d6ef9
PedroVe1oso <up201906293@up.pt>
2022-09-30 16:15:10 +0100

    Added code to have a moving X

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,14 +1,19 @@
 import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Game {
     private final Screen screen;
+    private int x = 10;
+    private int y = 10;
 
     public Game(int width, int height) throws IOException {
         Terminal terminal = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(width,height)).createTerminal();
@@ -20,12 +25,39 @@ public class Game {
     }
 
     public void run() throws IOException {
-        draw();
+        while(true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q')
+                screen.close();
+            if (key.getKeyType() == KeyType.EOF)
+                break;
+            processKey(key);
+        }
     }
 
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X') [0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X') [0]);
         screen.refresh();
+    }
+
+    private void processKey(KeyStroke key){
+        switch(key.getKeyType()){
+            case ArrowUp:
+                y--;
+                break;
+            case ArrowDown:
+                y++;
+                break;
+            case ArrowLeft:
+                x--;
+                break;
+            case ArrowRight:
+                x++;
+                break;
+            default:
+                System.out.println("No match");
+        }
     }
 }
