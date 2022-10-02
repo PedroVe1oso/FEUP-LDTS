f066eedfb221bf9e6c8662de5cab3f267f9b37d4
PedroVe1oso <up201906293@up.pt>
2022-10-02 14:45:00 +0100

    Added Monster class

---
+++ src/main/java/Monster.java
@@ -0,0 +1,18 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public class Monster extends Element{
+    public Monster(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void draw(TextGraphics graphics) {
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#f20f0f"));
+        graphics.setForegroundColor(TextColor.Factory.fromString("#F3F3F3"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(position.getX(), position.getY()), "M");
+    }
+}
