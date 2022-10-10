5dff37ade5f0b908f541abf43dbe7cab65c994a7
PedroVe1oso <up201906293@up.pt>
2022-10-10 23:29:36 +0100

    Added a helper method that initializes the list to all classes, using @BeforeEach annotation

--- numbers/src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ numbers/src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -1,16 +1,20 @@
 package com.aor.numbers;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class ListAggregatorTest {
+    private List<Integer> list;
+    @BeforeEach
+    public void setUpHelper (){
+        list = Arrays.asList(1,2,4,2,5);
+    }
     @Test
     public void sum() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
-
         ListAggregator aggregator = new ListAggregator();
         int sum = aggregator.sum(list);
 
@@ -19,8 +23,6 @@ public class ListAggregatorTest {
 
     @Test
     public void max() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
-
         ListAggregator aggregator = new ListAggregator();
         int max = aggregator.max(list);
 
@@ -29,8 +31,6 @@ public class ListAggregatorTest {
 
     @Test
     public void min() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
-
         ListAggregator aggregator = new ListAggregator();
         int min = aggregator.min(list);
 
@@ -39,8 +39,6 @@ public class ListAggregatorTest {
 
     @Test
     public void distinct() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
-
         ListAggregator aggregator = new ListAggregator();
         int distinct = aggregator.distinct(list);
 
