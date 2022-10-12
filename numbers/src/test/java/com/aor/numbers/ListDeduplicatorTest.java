9781c467acaa0a605bf701524eb7ab819fcaf681
PedroVe1oso <up201906293@up.pt>
2022-10-12 12:20:41 +0100

    All previous test that were altered are now implemented using mockito

--- numbers/src/test/java/com/aor/numbers/ListDeduplicatorTest.java
+++ numbers/src/test/java/com/aor/numbers/ListDeduplicatorTest.java
@@ -3,6 +3,7 @@ package com.aor.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,14 +18,10 @@ public class ListDeduplicatorTest {
     }
     @Test
     public void deduplicate() {
-        class StubListSorter implements GenericListSorter{
-            @Override
-            public List<Integer> sort(List<Integer> list) {
-                return Arrays.asList(1,2,2,4,5);
-            }
-        }
+        GenericListSorter sorter = Mockito.mock(GenericListSorter.class);
+        Mockito.when(sorter.sort(Mockito.anyList())).thenReturn(Arrays.asList(1,2,2,4,5));
 
-        ListDeduplicator deduplicator = new ListDeduplicator(new StubListSorter());
+        ListDeduplicator deduplicator = new ListDeduplicator(sorter);
         List<Integer> distinct = deduplicator.deduplicate(list);
 
         Assertions.assertEquals(expected, distinct);
@@ -32,14 +29,10 @@ public class ListDeduplicatorTest {
 
     @Test
     public void deduplicate_bug_8726() {
-        class StubListSorter implements GenericListSorter{
-            @Override
-            public List<Integer> sort(List<Integer> list) {
-                return Arrays.asList(1,2,2,4);
-            }
-        }
+        GenericListSorter sorter = Mockito.mock(GenericListSorter.class);
+        Mockito.when(sorter.sort(Mockito.anyList())).thenReturn(Arrays.asList(1,2,2,4));
 
-        GenericListDeduplicator deduplicator = new ListDeduplicator(new StubListSorter());
+        GenericListDeduplicator deduplicator = new ListDeduplicator(sorter);
         List<Integer> distinct = deduplicator.deduplicate(Arrays.asList(1,2,4,2));
 
         Assertions.assertEquals(Arrays.asList(1,2,4), distinct);
