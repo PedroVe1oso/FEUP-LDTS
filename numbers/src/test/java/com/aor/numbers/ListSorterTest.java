9e522ac79ab62639d98a005c8175fa663ad14103
PedroVe1oso <up201906293@up.pt>
2022-10-12 11:42:27 +0100

    Added sort_bug_8726 test (does not pass)

--- numbers/src/test/java/com/aor/numbers/ListSorterTest.java
+++ numbers/src/test/java/com/aor/numbers/ListSorterTest.java
@@ -17,10 +17,17 @@ public class ListSorterTest {
     }
     @Test
     public void sort() {
-
         ListSorter sorter = new ListSorter();
         List<Integer> sorted = sorter.sort(list);
 
         Assertions.assertEquals(expected, sorted);
+    }
+
+    @Test
+    public void sort_bug_8726() {
+        ListSorter sorter = new ListSorter();
+        List<Integer> sorted = sorter.sort(Arrays.asList(1,2,4,2));
+
+        Assertions.assertEquals(Arrays.asList(1,2,2,4), sorted);
     }
 }
