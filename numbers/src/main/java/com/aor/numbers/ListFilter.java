89bd5f09d590188838f1c95f8e1d8588eaec0d27
PedroVe1oso <up201906293@up.pt>
2022-10-12 12:48:48 +0100

    Added GenericListFilter interface and ListFilter class

---
+++ numbers/src/main/java/com/aor/numbers/ListFilter.java
@@ -0,0 +1,15 @@
+package com.aor.numbers;
+
+import java.util.List;
+
+public class ListFilter{
+    GenericListFilter filter;
+
+    public ListFilter(GenericListFilter filter) {
+        this.filter = filter;
+    }
+
+    public List<Integer> filter(List<Integer> list) {
+        return null;
+    }
+}
