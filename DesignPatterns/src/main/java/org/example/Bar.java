c6dabf763d4c82d222ce9d438e02f7dfcf35ce6b
PedroVe1oso <up201906293@up.pt>
2022-11-08 18:09:18 +0000

    Added the add, remove, and notify observer methods to the bar class

--- DesignPatterns/src/main/java/org/example/Bar.java
+++ DesignPatterns/src/main/java/org/example/Bar.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Bar {
     protected boolean isHappyHour;
+    protected List<BarObserver> observers;
+
+    public Bar() {
+        this.isHappyHour = false;
+        observers = new ArrayList<>();
+    }
 
     public boolean getIsHappyHour() {
         return isHappyHour;
@@ -9,6 +18,18 @@ public abstract class Bar {
 
     public void setIsHappyHour(boolean isHappyHour) {
         this.isHappyHour = isHappyHour;
+    }
+
+    public void addObserver(BarObserver observer) {
+        observers.add(observer);
+    }
+    public void removeObserver(BarObserver observer) {
+        observers.remove(observer);
+    }
+    public void notifyObservers() {
+        for (BarObserver observer : observers)
+            if (isHappyHour()) observer.happyHourStarted(this);
+            else observer.happyHourEnded(this);
     }
 
     abstract public boolean isHappyHour();
--- DesignPatterns/src/main/java/org/example/StringBar.java
+++ DesignPatterns/src/main/java/org/example/StringBar.java
@@ -1,21 +1,14 @@
 package org.example;
 
 public class StringBar extends Bar{
-
-    public StringBar() {
-        isHappyHour = false;
-    }
-
     @Override
     public boolean isHappyHour() {
         return getIsHappyHour();
     }
-
     @Override
     public void startHappyHour() {
         setIsHappyHour(true);
     }
-
     @Override
     public void endHappyHour() {
         setIsHappyHour(false);
