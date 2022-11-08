d79c2f32a125d5c963fc8e7d17bbdf76219f583c
PedroVe1oso <up201906293@up.pt>
2022-11-08 10:17:23 +0000

    Added String Replacer class that implements the String Transformer interface and the string replacer test now passes

---
+++ DesignPatterns/src/main/java/org/example/StringReplacer.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class StringReplacer implements StringTransformer{
+
+    private char a;
+    private char b;
+
+    public StringReplacer(char a, char b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public void execute(StringDrink drink) {
+        String str = drink.getText().replace(a, b);
+        drink.setText(str);
+    }
+}
